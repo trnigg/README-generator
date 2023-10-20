925b807f3726660a05c67d70d63aefa19f624952
trnigg <tim@nigg.com.au>
2023-10-20 14:12:43 +1100

    Added Inquirer question sequence for readme content

--- index.js
+++ index.js
@@ -6,75 +6,144 @@ var fs = require('fs');
 
 // GLOBAL VARIABLES for console text colours. Combined Module 9 Activity 22 with guide here: 
 // https://dev.to/ifenna__/adding-colors-to-bash-scripts-48g4
+//TODO [TN]: Create an key:value object for different text colours
 const redTextCol = '\x1b[31m';
 const greenTextCol = '\x1b[32m';
 const yellowTextCol = '\x1b[33m';
+const greyTextCol = '\x1b[90m';
 const endTextCol = '\x1b[0m';
 
+const licenseArray = [
+    'GNU AGPLv3',
+    'GNU GPLv3',
+    'GNU LGPLv3',
+    'Mozilla Public License 2.0',
+    'Apache License 2.0',
+    'MIT License',
+    'Boost Software License 1.0',
+    'The Unlicense'
+];
 
-const instructions = "You will be asked a number of questions to help generate your README. Questions with answer that default to N/A can be skipped (left blank) and subsequently won't generate the relevant section in your README."
+const instructions = "You will be asked a number of questions to help generate your README; all questions must be answered."
 const confirmInstructions = 
     {
         type: 'confirm',
         name: 'understood',
-        message: 'Please confirm you understand and wish to continue:',
+        message: 'Please confirm that you understand and wish to continue:',
+        default: false,
+        // validate: (understood) => {
+        //     if (understood !== 'y' || understood !== 'n') {
+        //       return 'Cannot be left blank. Please enter a title.'
+        //     }
+        //     return true;
+        // } 
     };
 
-
-// TODO: Create an array of questions for user input
+// TODO [TN]: Confirm arrows are okay within this array/objects
+// FIX validation for 'confirm questions'
 const readmeQuestions = [
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
+        validate: (title) => {
+            if (!title.length) {
+              return 'Cannot be left blank. Please enter a title.'
+            }
+            return true;
+        } 
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+        validate: (username) => {
+            if (!username.length) {
+              return 'Cannot be left blank. Please enter a username.'
+            }
+            return true;
+        } 
     },
     {
         type: 'input',
         name: 'description',
         message: 'Please briefly describe your project:',
-    },
-    {
-        type: 'checkbox',
-        name: 'languages',
-        message: 'What languages do you know?',
-        choices: ['HTML', 'CSS', 'JavaScript', 'English', 'LOTE']
+        validate: (description) => {
+            if (!description.length) {
+              return 'Cannot be left blank. Please enter a description.'
+            }
+            return true;
+        } 
     },
     {
         type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
+        name: 'installation',
+        message: 'Please briefly describe how to install the application:',
+        validate: (installation) => {
+            if (!installation.length) {
+              return 'Cannot be left blank. Please enter an installation guide.'
+            }
+            return true;
+        } 
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please briefly describe how to use the application:',
+        validate: (usage) => {
+            if (!usage.length) {
+              return 'Cannot be left blank. Please enter a usage guide.'
+            }
+            return true;
+        } 
+    },
+    {
+        type: 'confirm',
+        name: 'acceptingContribution',
+        default: false,
+        message: 'Are you currently looking for contributions to your project?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please briefly describe how someone can contribute to your project:',
+        when: (responses) => responses.acceptingContribution,
+    },
+    {
+        type: 'confirm',
+        name: 'includeLicense',
+        default: false,
+        message: 'Does your project have a license?',
+    },
+    {
+        type: 'list',
+        name: 'licenseType',
+        message: 'Please select the license that applies to your project:',
+        choices: licenseArray,
+        default: 'MIT',
+        when: (responses) => responses.includeLicense,
     },
 ];
 
+// Following guides is comprehensive account (beyond the docs) of how to use Inquirer: 
+// https://javascript.plainenglish.io/how-to-inquirer-js-c10a4e05ef1f
+// Also helpful: https://geshan.com.np/blog/2023/03/inquirer-js/ 
 
 function askQuestions() {
     inquirer
     .prompt(confirmInstructions)
-    .then((response) => {
-        if (response.understood) {
+    .then((confirmResponse) => {
+        if (confirmResponse.understood) {
             console.log(`${greenTextCol}Thank you.${endTextCol}`);
             inquirer
             .prompt(readmeQuestions)
-            .then((response) =>
-                console.log(response));
+            .then((responses) =>
+                console.log(responses));
         } else {
             console.log(`${redTextCol}Ending the README generator.${endTextCol} Please feel free to start again by entering ${yellowTextCol}'node index.js'${endTextCol} in your console.`);
             return;
         }
-    }
-        
-    );
-  
-    function generateHTML(response) {
-        let languagesList = '';
-    
-        // Loop through the selected languages and create a list item for each
-        for (const language of response.languages) {
-            languagesList += `<li>${language}</li>`;
-        }
-        return `
-        `
-    }
+    });
 }
 
 
