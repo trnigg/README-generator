1d3897bcbed5a44bfcb25649f01ba04a129a9350
trnigg <tim@nigg.com.au>
2023-10-24 11:20:01 +1100

    Added feature to render badge with link to license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,18 +3,19 @@
 // [TR] TODO: Refactor method, abbrev. license names
 function renderLicenseBadge(license) {
   const licenseBadges = {
-    'GNU AGPLv3': 'GNU_AGPLv3' ,
-    'GNU GPLv3': 'GNU_GPLv3',
-    'GNU LGPLv3': 'GNU_LGPLv3' ,
-    'Mozilla Public License 2.0': 'Mozilla_Public_License_2.0' ,
-    'Apache License 2.0' : 'Apache_License_2.0' ,
-    'MIT License' : 'MIT_License' ,
-    'Boost Software License 1.0' : 'Boost_Software_License_1.0' ,
-    'The Unlicense' : 'The_Unlicense' ,
+    'GNU AGPLv3': 'AGPL_3.0' ,
+    'GNU GPLv3': 'GPL_3.0',
+    'GNU LGPLv3': 'LGPL_3.0' ,
+    'Mozilla Public License 2.0': 'MPL_2.0' ,
+    'Apache License 2.0' : 'Apache_2.0' ,
+    'MIT License' : 'MIT' ,
+    'Boost Software License 1.0' : 'BSL_1.0' ,
+    'The Unlicense' : 'Unlicense' ,
   } // Check if license is a key within object of licenseBadges and get the resulting badge URL and combine with link
   if (licenseBadges[license]) {
-  return `[${license}](https://img.shields.io/badge/License-${licenseBadges[license]}-blue)(${renderLicenseLink(license)}).`;
-  }
+  return `[![${license}](https://img.shields.io/badge/License-${licenseBadges[license]}-blue)](#license)`;
+  } 
+  return '';
 }
 
 // TODO: Create a function that returns the license link
@@ -36,32 +37,35 @@ function renderLicenseLink(license) {
     //TODO : rather than template literal, add the appendage to the relevant license link
     //maybe : licenseLink += licenseAppendages[license] - need to test before implementation
     return `This project is licensed under [${license}](${licenseLink}${licenseAppendages[license]}).`;
-  } else {
-    return '';
   }
-
+  return '';
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `
-  ## License
-  ${renderLicenseLink(license)}
-  `;
+  if (license) {
+    return `
+    ## License
+    ${renderLicenseLink(license)}
+    `;
+  }
+  return '';
 }
   
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // Declare and trim leading/trailing whitespaces for better readme (esp. when empty strings above are returned)
+  const licenseBadge = renderLicenseBadge(data.licenseType).trim();
+  const licenseSection = renderLicenseSection(data.licenseType).trim();
 
+  // Render to follow - note, does not follow indentation convention to avoid whitespaces in readme.
   return `# ${data.title}
-
-  ${renderLicenseBadge(data.licenseType)}
-
-  ${renderLicenseSection(data.licenseType)}
-
+${licenseBadge}
+${licenseSection}
 `;
+// [TR] TODO - post-process remove the leading whitespaces on lines before returning
 }
 
 module.exports = generateMarkdown;
